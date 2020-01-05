d81a1fc1bf0304f3d8691346e8f136fe6c13254d
Carly <carly.jenkinson65@googlemail.com>
2020-01-05 18:52:46 +0000

    Refactor tests.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -10,38 +10,16 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('can add ingredient', () => {
-  const { getByText, getByLabelText } = render(<App />);
-  const ingredient = getByLabelText('Ingredient:')
-  fireEvent.change(ingredient, { target: { value: 'Apple' } })
-  fireEvent.click(getByText('Add'))
-  expect(ingredient.value).toBe('');
-  expect(getByText('Apple')).toBeDefined();
-});
-
-it('can add two ingredients', () => {
-  const { getByText, getByLabelText } = render(<App />);
-  const ingredient = getByLabelText('Ingredient:')
-  fireEvent.change(ingredient, { target: { value: 'Apple' } })
-  fireEvent.click(getByText('Add'))
-  fireEvent.change(ingredient, { target: { value: 'Banana' } })
-  fireEvent.click(getByText('Add'))
-  expect(getByText('Apple')).toBeDefined();
-  expect(getByText('Banana')).toBeDefined();
-});
-
-it('has a label for quantity', () => {
-  const { getByLabelText } = render(<App />);
-  expect(getByLabelText('Quantity:').value).toEqual('');
-});
-
-it('can add quantity of ingredient', () => {
+it('can add more than one ingredient', () => {
   const { getByText, getByLabelText } = render(<App />);
   const ingredient = getByLabelText('Ingredient:')
   const quantity = getByLabelText('Quantity:')
   fireEvent.change(ingredient, { target: { value: 'Apple' } })
   fireEvent.change(quantity, { target: { value: '1' } })
   fireEvent.click(getByText('Add'))
+  fireEvent.change(ingredient, { target: { value: 'Banana' } })
+  fireEvent.change(quantity, { target: { value: '2' } })
+  fireEvent.click(getByText('Add'))
   expect(quantity.value).toBe('');
-  expect(getByText('1 Apple')).toBeDefined();
+  expect(getByText('2 Banana')).toBeDefined();
 });
