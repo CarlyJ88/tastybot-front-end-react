372e36d0720abd8173a621f873d122ec125bb71f
CarlyJ88 <carly.jenkinson65@googlemail.com>
2020-01-14 14:37:38 +0000

    Can add multiple ingredients.

--- src/RecipeInput.jsx
+++ src/RecipeInput.jsx
@@ -2,18 +2,34 @@ import React, { useState, Fragment } from 'react';
 
 function RecipeInput() {
     const [ingredient, setIngredient] = useState();
+    const [showIngredient, setShowIngredient] = useState([]);
+    const changeHandler = ev => {
+        ev.preventDefault();
+        setIngredient(ev.currentTarget.value)
+    }
 
     return (
         <Fragment>
-        <label>
-            Ingredient:
+            <label>
+                Ingredient:
             <input
-                type="text"
-                value={ingredient}
-                onChange={setIngredient}
-            />
-        </label>
-        <button type="button" onClick={()=>alert('Hello world!')}>Add</button>
+                    type="text"
+                    value={ingredient}
+                    onChange={changeHandler}
+                />
+            </label>
+            <button type="button" onClick={() => {
+                setShowIngredient([...showIngredient, ingredient]);
+                setIngredient('');
+            }}>Add</button>
+
+            <div>
+                <ul>
+                    {showIngredient.map((item, index) => {
+                    return <li key= {index}>{item}</li>
+                    })}
+                </ul>
+            </div>
         </Fragment>
     );
 }
--- src/RecipeInput.test.jsx
+++ src/RecipeInput.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RecipeInput from './RecipeInput';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 it('has a label for ingredient', () => {
@@ -12,4 +12,24 @@ it('has a label for ingredient', () => {
   it('has a add ingredient button', () => {
     const { getByText } = render(<RecipeInput />);
     expect(getByText('Add').constructor.name).toEqual('HTMLButtonElement');
+  });
+
+  it('can add ingredient', () => {
+    const { getByText, getByLabelText } = render(<RecipeInput />);
+    const ingredient = getByLabelText('Ingredient:')
+    fireEvent.change(ingredient, { target: { value: 'Apple' } })
+    fireEvent.click(getByText('Add'))
+    // expect(ingredient.value).toBe('');
+    expect(getByText('Apple')).toBeDefined();
+  });
+
+  it('can add two ingredients', () => {
+    const { getByText, getByLabelText } = render(<RecipeInput />);
+    const ingredient = getByLabelText('Ingredient:')
+    fireEvent.change(ingredient, { target: { value: 'Apple' } })
+    fireEvent.click(getByText('Add'))
+    fireEvent.change(ingredient, { target: { value: 'Banana' } })
+    fireEvent.click(getByText('Add'))
+    expect(getByText('Apple')).toBeDefined();
+    expect(getByText('Banana')).toBeDefined();
   });
