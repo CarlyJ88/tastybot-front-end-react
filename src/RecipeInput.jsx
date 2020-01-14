d06335f5a8646adab7388038a994ef9479455cfe
CarlyJ88 <carly.jenkinson65@googlemail.com>
2020-01-14 18:23:40 +0000

    Can add quantity of ingredient. Has a label for unit. Can add unit of ingredient.

--- src/RecipeInput.jsx
+++ src/RecipeInput.jsx
@@ -3,10 +3,19 @@ import React, { useState, Fragment } from 'react';
 function RecipeInput() {
     const [ingredient, setIngredient] = useState();
     const [quantity, setQuantity] = useState();
+    const [unit, setUnit] = useState();
     const [showIngredient, setShowIngredient] = useState([]);
     const changeHandler = ev => {
         ev.preventDefault();
         setIngredient(ev.currentTarget.value)
+    }
+    const quantityChangeHandler = ev => {
+        ev.preventDefault();
+        setQuantity(ev.currentTarget.value)
+    }
+    const unitChangeHandler = ev => {
+        ev.preventDefault();
+        setUnit(ev.currentTarget.value)
     }
 
     return (
@@ -24,18 +33,27 @@ function RecipeInput() {
                 <input 
                 type="text"
                 value={quantity}
-                onChange={changeHandler}
+                onChange={quantityChangeHandler}
+                />
+            </label>
+            <label>
+                Unit:
+                <input
+                type="text"
+                value={unit}
+                onChange={unitChangeHandler}
                 />
             </label>
             <button type="button" onClick={() => {
-                setShowIngredient([...showIngredient, ingredient]);
+                setShowIngredient([...showIngredient, {ingredient, quantity, unit}]);
                 setIngredient('');
+                setQuantity('');
             }}>Add</button>
 
             <div>
                 <ul>
                     {showIngredient.map((item, index) => {
-                    return <li key= {index}>{item}</li>
+                    return <li key= {index}>{item.quantity}{item.unit} {item.ingredient}</li>
                     })}
                 </ul>
             </div>
--- src/RecipeInput.test.jsx
+++ src/RecipeInput.test.jsx
@@ -29,7 +29,31 @@ import '@testing-library/jest-dom/extend-expect';
     expect(getByText('Banana')).toBeDefined();
   });
 
-  it('has a label for quantity', () => {
+  it('can add quantity of ingredient', () => {
+    const { getByText, getByLabelText } = render(<RecipeInput />);
+    const ingredient = getByLabelText('Ingredient:')
+    const quantity = getByLabelText('Quantity:')
+    fireEvent.change(ingredient, { target: { value: 'Apple' } })
+    fireEvent.change(quantity, { target: { value: '1' } })
+    fireEvent.click(getByText('Add'))
+    expect(quantity.value).toBe('');
+    expect(getByText('1 Apple')).toBeDefined();
+  });
+
+  it('has a label for unit', () => {
     const { getByLabelText } = render(<RecipeInput />);
-    expect(getByLabelText('Quantity:').value).toEqual('');
+    expect(getByLabelText('Unit:').value).toEqual('');
+  });
+
+  it('can add unit of ingredient', () => {
+    const { getByText, getByLabelText } = render(<RecipeInput />);
+    const ingredient = getByLabelText('Ingredient:')
+    const quantity = getByLabelText('Quantity:')
+    const unit = getByLabelText('Unit:')
+    fireEvent.change(ingredient, { target: { value: 'Apple' } })
+    fireEvent.change(quantity, { target: { value: '100' } })
+    fireEvent.change(unit, { target: { value: 'g' } })
+    fireEvent.click(getByText('Add'))
+    expect(quantity.value).toBe('');
+    expect(getByText('100g Apple')).toBeDefined();
   });
