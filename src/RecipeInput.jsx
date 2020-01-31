6885a062eb25c6c76158f662732c7e6e61dcb2c6
CarlyJ88 <carly.jenkinson65@googlemail.com>
2020-01-31 11:56:50 +0000

    Refactored component and removed unused imports.

--- src/RecipeInput.jsx
+++ src/RecipeInput.jsx
@@ -1,62 +1,10 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import IngredientsPage from './IngredientsPage';
 
 function RecipeInput() {
-    const [ingredient, setIngredient] = useState();
-    const [quantity, setQuantity] = useState();
-    const [unit, setUnit] = useState();
-    const [showIngredient, setShowIngredient] = useState([]);
-    const changeHandler = ev => {
-        ev.preventDefault();
-        setIngredient(ev.currentTarget.value)
-    }
-    const quantityChangeHandler = ev => {
-        ev.preventDefault();
-        setQuantity(ev.currentTarget.value)
-    }
-    const unitChangeHandler = ev => {
-        ev.preventDefault();
-        setUnit(ev.currentTarget.value)
-    }
-
     return (
         <Fragment>
-            <label>
-                Ingredient:
-            <input
-                    type="text"
-                    value={ingredient}
-                    onChange={changeHandler}
-                />
-            </label>
-            <label>
-                Quantity:
-                <input 
-                type="text"
-                value={quantity}
-                onChange={quantityChangeHandler}
-                />
-            </label>
-            <label>
-                Unit:
-                <input
-                type="text"
-                value={unit}
-                onChange={unitChangeHandler}
-                />
-            </label>
-            <button type="button" onClick={() => {
-                setShowIngredient([...showIngredient, {ingredient, quantity, unit}]);
-                setIngredient('');
-                setQuantity('');
-            }}>Add</button>
-
-            <div>
-                <ul>
-                    {showIngredient.map((item, index) => {
-                    return <li key= {index}>{item.quantity}{item.unit} {item.ingredient}</li>
-                    })}
-                </ul>
-            </div>
+       <IngredientsPage/>
         </Fragment>
     );
 }
