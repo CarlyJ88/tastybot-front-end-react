ceea5a6c5008bd5ac0b71474e6678a083f36b11f
Carly <carly.jenkinson65@googlemail.com>
2022-06-28 16:39:41 +0100

    Show ingredient category.

--- src/ListIngredients.jsx
+++ src/ListIngredients.jsx
@@ -8,7 +8,7 @@ function ListIngredients({ showIngredient, handleRemove, onClick }) {
       <ul className="list-ingredients">
         {showIngredient.map((item, index) => {
           console.log(item, 'item')
-          return <li className="item" key={index} >{item.quantity}{item.unit} <a href={item.link}>{item.ingredient_name}</a> <button type="button" className="button" onClick={() => handleRemove(index)}>Remove</button></li>
+          return <li className="item" key={index} >{item.quantity}{item.unit} <a href={item.link}>{item.ingredient_name}</a> {item.categories} <button type="button" className="button" onClick={() => handleRemove(index)}>Remove</button></li>
         })}
       </ul>
       <input className="add-ingredient" type="submit" value="Add Ingredient" onClick={onClick} />
